6570f0cf0727701bd3ea3a40319f6d9d8f69a2a9
Maaz Haq <Maaz_haq123@hotmail.com>
2018-03-05 13:24:17 -0500

    added spring physics, also added a clamped rotation for the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ConfigurableJoint))]
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
 
@@ -9,10 +10,25 @@ public class PlayerController : MonoBehaviour {
 	private float m_speed = 5.0f;
 	[SerializeField]
 	private float lookSensitivity = 3.0f;
+	[SerializeField]
+	private float m_thrusterForce = 1000.0f;
+
+	[Header("Spring Settings")]
+	[SerializeField]
+	private JointDriveMode m_jointMode = JointDriveMode.Position;
+	[SerializeField]
+	private float m_jointSpring = 20.0f;
+	[SerializeField]
+	private float m_jointMaxForce = 40.0f;
+
 	private PlayerMotor m_motor;
+	private ConfigurableJoint m_joint;
 
 	void Start() {
 		m_motor = GetComponent<PlayerMotor>();
+		m_joint = GetComponent<ConfigurableJoint>();
+
+		SetJointSettings(m_jointSpring);
 	}
 
 	void Update() {
@@ -40,11 +56,29 @@ public class PlayerController : MonoBehaviour {
 		//calculate camera rotation as a 3d Vector (turning around)
 		float xRotation = Input.GetAxisRaw("Mouse Y");
 
-		Vector3 cameraRotation = new Vector3(xRotation, 0.0f, 0.0f) * lookSensitivity;
+		float cameraRotationX = xRotation * lookSensitivity;
 
 		//Apply rotation
-		m_motor.RotateCamera(cameraRotation);
+		m_motor.RotateCamera(cameraRotationX);
 
+		//Calculate the thruster force based on input
+		Vector3 thrusterForce = Vector3.zero;
+		if(Input.GetButton("Jump")) {
+			thrusterForce = Vector3.up * m_thrusterForce;
+			SetJointSettings(0f);
+		} else {
+			SetJointSettings(m_jointSpring);
+		}
+		
+		//Apply Thruster force
+		m_motor.ApplyThruster(thrusterForce);
+	}
 
+	private void SetJointSettings(float jointSpring) {
+		m_joint.yDrive = new JointDrive { 
+			mode = m_jointMode, 
+			positionSpring = jointSpring,
+			maximumForce = m_jointMaxForce 
+		};
 	}
 }
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 public class PlayerMotor : MonoBehaviour {
 	[SerializeField]
 	private Camera cam;
+	[SerializeField]
+	private float m_cameraRotationLimit = 85.0f;
+	private float m_cameraRotationX = 0.0f;
+	private float m_currentCameraRotationX = 0.0f;
 	private Vector3 m_velocity = Vector3.zero;
 	private Vector3 m_rotation = Vector3.zero;
-	private Vector3 m_cameraRotation = Vector3.zero;
+	private Vector3 m_thrusterForce = Vector3.zero;
 	private Rigidbody rb;
 
 	void Start() {
@@ -24,8 +28,13 @@ public class PlayerMotor : MonoBehaviour {
 		m_rotation = rotation;
 	}
 
-	public void RotateCamera(Vector3 cameraRotation) {
-		m_cameraRotation = cameraRotation;
+	public void RotateCamera(float cameraRotationX) {
+		m_cameraRotationX = cameraRotationX;
+	}
+
+	//Get a force vector for our thrusters
+	public void ApplyThruster(Vector3 thrusterForce) {
+		m_thrusterForce = thrusterForce;
 	}
 
 	//Runs every physics iteration
@@ -39,12 +48,21 @@ public class PlayerMotor : MonoBehaviour {
 		if (m_velocity != Vector3.zero) {
 			rb.MovePosition(rb.position + m_velocity * Time.fixedDeltaTime);
 		}
+
+		if(m_thrusterForce != Vector3.zero) {
+			rb.AddForce(m_thrusterForce * Time.fixedDeltaTime, ForceMode.Acceleration);
+		}
 	}
 
 	void PerformRotation() {
 		rb.MoveRotation(rb.rotation * Quaternion.Euler(m_rotation));
 		if(cam != null) {
-			cam.transform.Rotate(-m_cameraRotation);
+			//Set our rotation and clamp it
+			m_currentCameraRotationX -= m_cameraRotationX;
+			m_currentCameraRotationX = Mathf.Clamp(m_currentCameraRotationX, -m_cameraRotationLimit, m_cameraRotationLimit);
+
+			//Apply our rotation to the transform of our camera
+			cam.transform.localEulerAngles = new Vector3(m_currentCameraRotationX, 0.0f, 0.0f);
 		}
 	}
 
